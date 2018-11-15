2dda0a3a7846b6a8f6daf098028bd7e14f6a2790
Ovidiu Ivanica <ovidiu.ivanica@nxp.com>
2018-11-16 00:02:28 +0200

    enable rp control over power relays

--- manual.py
+++ manual.py
@@ -6,6 +6,10 @@ if __name__ == '__main__':
     lck = Lock()
     logging.basicConfig(level=logging.INFO)
     board = RelayBoard(lck)
+    # activate raspberry control over main power relays
+    board.startRelay(8)
+
+    # enable 3 rooms expcept kitchen
     board.startRelay(1)
     board.startRelay(2)
     board.startRelay(3)
