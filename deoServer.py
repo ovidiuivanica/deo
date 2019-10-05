9a302e3b4f571404525d9985115e6687ae5ae4d3
Ovidiu Ivanica <ovidiu.ivanica@nxp.com>
2019-10-05 19:29:55 +0300

    show room data

--- deoServer.py
+++ deoServer.py
@@ -182,6 +182,7 @@ class Yard:
 def controllerLogic(room, board, prag=0.0):
 
     #persistantChek(room)
+    logging.debug("room: %s", json.dumps(room, indent=4))
 
     if (room.get("temperature") + prag) < room.get("reference"):
         if room.get("heater") == True:
