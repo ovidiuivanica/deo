343b234f4ef364ef06949c9c6bae46eb025f9e1d
Ovidiu Ivanica <ovidiu.ivanica@nxp.com>
2019-10-07 22:13:56 +0300

    fix funrace active state

--- deoServer.py
+++ deoServer.py
@@ -128,14 +128,14 @@ class Furnace:
         self.active = False
 
     def start(self):
-        __active = True if GPIO.input(self.board.pinout[self.id]) else False
+        __active = False if GPIO.input(self.board.pinout[self.id]) else True
         if not __active:
             self.board.startRelay(self.id)
             logging.info("[Furnace] starting")
         self.active = True
 
     def stop(self):
-        __active = True if GPIO.input(self.board.pinout[self.id]) else False
+        __active = False if GPIO.input(self.board.pinout[self.id]) else True
         if __active:
             self.board.stopRelay(self.id)
             logging.info("[Furnace] stopping")
